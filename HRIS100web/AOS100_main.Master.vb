ef934c29460b6167ae0aaf3ecce7867b4460af29
Freddierick Santos <freddiericksantos@gmail.com>
2024-08-19 18:56:16 +0800

    Revert the testing

--- HRIS100web/AOS100_main.Master.vb
+++ HRIS100web/AOS100_main.Master.vb
@@ -70,7 +70,7 @@ Public Class aOS100_main
 				txtShtCut.Text = Format(CDate(vTransYear), "yyyy-MM-dd")
 
 			Case Else
-				txtShtCut.Text = "Not Found...1234567"
+				txtShtCut.Text = "Not Found..."
 
 		End Select
 	End Sub
